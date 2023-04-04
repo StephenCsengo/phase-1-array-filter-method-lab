fa00c6cbd2049720a08a21d37016b326896da718
Stephen Csengo <stephencsengo@gmail.com>
2023-04-04 13:55:22 -0400

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, search) {
+    return drivers.filter((element) => element.toLowerCase().includes(search.toLowerCase()));
+}
+
+function fuzzyMatch(drivers, search){
+    return drivers.filter((element) => element.toLowerCase().startsWith(search.toLowerCase()));
+}
+
+function matchName(drivers, search){
+    return drivers.filter((element) => element.name.includes(search));
+}
